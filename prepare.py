7f6faa4bc75517c9b67f075cbf02271b6204f060
peroon <fromjpn@gmail.com>
2017-04-05 23:58:54 +0900

    Generate lst file

--- prepare.py
+++ prepare.py
@@ -4,12 +4,14 @@ import glob
 import cv2
 import os
 import shutil
+from collections import deque
 
 from tqdm import tqdm
 
 from constant import IMAGES_ROOT
 from constant import IMAGE_SIZE
 from square_cropper import SquareCropper
+
 
 def make_directory():
     os.mkdir(IMAGES_ROOT + 'clf_train_all')
@@ -26,11 +28,15 @@ def make_directory():
 
 
 def __new_filename(filename='train_1.jpg'):
+    """元々のファイル名を 8桁 zero paddingしたファイル名に変更"""
+
     image_id = int(filename.split('.')[0].split('_')[-1])
     return '%08d' % image_id + '.jpg'
 
 
 def copy_and_rename():
+    """元々の画像ファイルをリネームしながら1フォルダにまとめる"""
+
     for name in ['train', 'test']:
         image_path_list = glob.glob(IMAGES_ROOT + 'clf_{}_*/*.jpg'.format(name))
         output_directory = IMAGES_ROOT + 'clf_{}_all/'.format(name)
@@ -44,6 +50,8 @@ def copy_and_rename():
 
 
 def crop():
+    """画像をcrop, resizeする"""
+
     cropper = SquareCropper()
 
     for name in ['train', 'test']:
@@ -57,12 +65,82 @@ def crop():
                 cv2.imwrite(save_dir + filename, cropped_image)
 
 
-def resize():
-    pass
+def split_list(list_input, fold_num=5, fold_index=0):
+    """split data for train and validation"""
+
+    num_all = len(list_input)
+    num_val = int(num_all / fold_num)
+    num_tra = num_all - num_val
+
+    deq = deque(list_input)
+    deq.rotate(fold_index * num_val)
+    return list(deq)[:num_tra], list(deq)[num_tra:]
 
 
-def make_lst():
-    pass
+def get_labels():
+    labels = []
+    with open(IMAGES_ROOT + 'clf_train_master.tsv', 'r') as f:
+        f.readline()
+        for s in f:
+            label = int(s.split()[-1])
+            labels.append(label)
+    return labels
+
+
+def write_lst(save_path, image_path_list, labels):
+    f = open(save_path, 'w')
+    for i in range(len(image_path_list)):
+        s = '{}\t{}\t{}\n'.format(i, labels[i], image_path_list[i])
+        f.write(s)
+    f.close()
+
+
+def make_train_lst():
+    labels = get_labels()
+    os.mkdir(IMAGES_ROOT + 'mxnet')
+    mxnet_dir = IMAGES_ROOT + 'mxnet/'
+
+    for fold_i in range(5):
+        # foldごとにデータリストを作る
+        image_path_list_train_all = []
+        image_path_list_validation_all = []
+        labels_train_all = []
+        labels_validation_all = []
+
+        for crop_i in range(4):
+            glob_path = IMAGES_ROOT + 'clf_train_224x224_{}/*'.format(crop_i)
+            image_path_list = glob.glob(glob_path)
+
+            # train/valにsplitする
+            image_path_list_train, image_path_list_validation = split_list(image_path_list, fold_index=fold_i)
+            labels_train, labels_validation = split_list(labels, fold_index=fold_i)
+
+            # allに加える
+            image_path_list_train_all += image_path_list_train
+            image_path_list_validation_all += image_path_list_validation
+            labels_train_all += labels_train
+            labels_validation_all += labels_validation
+
+        # ファイルに書き込む
+        train_lst_path = mxnet_dir + 'train_224x224_fold_{}.lst'.format(fold_i)
+        write_lst(train_lst_path, image_path_list_train_all, labels_train_all)
+
+        validation_lst_path = mxnet_dir + 'validation_224x224_fold_{}.lst'.format(fold_i)
+        write_lst(validation_lst_path, image_path_list_validation_all, labels_validation_all)
+
+
+def make_test_lst():
+    mxnet_dir = IMAGES_ROOT + 'mxnet/'
+
+    # 各cropごとに1ファイルにする
+    for crop_i in range(0, 4):
+        glob_path = IMAGES_ROOT + 'clf_test_224x224_{}/*'.format(crop_i)
+        image_path_list = glob.glob(glob_path)
+        labels = [0] * len(image_path_list)
+
+        # ファイルに書き込む
+        lst_path = mxnet_dir + 'test_224x224_crop_{}.lst'.format(crop_i)
+        write_lst(lst_path, image_path_list, labels)
 
 
 def make_rec():
@@ -72,7 +150,7 @@ def make_rec():
 if __name__ == '__main__':
     #make_directory()
     #copy_and_rename()
-    crop()
-    #resize()
-    #make_lst()
+    #crop()
+    make_train_lst()
+    make_test_lst()
     #make_rec()
